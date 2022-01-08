e6a095334ba6049b24059d1c8e0144189016ec6b
Cameron Wilson-Rasch <cmwilsonrasch@gmail.com>
2022-01-08 09:25:32 -0600

    figured out the test after fiddling in with console.log. needed to move into my test environment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,21 @@
 const removeFromArray = function(arr, ...vals) {
+  let paramVal;
+  let arrVal;
+
   for (const val of vals) {
-    index = arr.indexOf(val)
-  } 
-  if (index > -1) {
-    arr.splice(index, vals.length);
+    index = vals.indexOf(val)
+    paramVal = vals[index];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      arrVal = arr[i]
+
+      if (paramVal === arrVal) {
+        arr.splice(arr.indexOf(arrVal), 1);
+      }
+    }
   }
   return arr;
 };
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
