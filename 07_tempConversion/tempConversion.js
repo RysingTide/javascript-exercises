0868f7b269d8932f61f771995386b01a2573ec88
Cameron Wilson-Rasch <cmwilsonrasch@gmail.com>
2022-01-09 13:55:38 -0600

    passed first test converting fahrenheit to celcius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celcius = (fahrenheit - 32) * (5/9);
+  if (celcius % 1 !== 0) {
+    return celcius.toFixed(1);
+  } else {
+    return celcius;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  fahrenheit = (celcius * 9/5) + 32;
+  if (fahrenheit % 1 !== 0) {
+    return fahrenheit.toFixed(1);
+  }
+  return fahrenheit;
 };
 
 // Do not edit below this line
