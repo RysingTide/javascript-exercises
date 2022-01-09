721e41937e70a5a832de02c9643bc50bbd96a8b5
Cameron Wilson-Rasch <cmwilsonrasch@gmail.com>
2022-01-09 11:39:06 -0600

    completed function that passes first test in 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isLeapYear = (year) => {
+    if (year % 4 !== 0) {
+      return false;
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
+  if(isLeapYear(year)) {
+    return true;
+  }
 };
 
 // Do not edit below this line
