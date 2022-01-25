62497543f0b7051e6a25a676a9a98a0f86e273f2
Cameron Wilson-Rasch <cmwilsonrasch@gmail.com>
2022-01-25 15:07:10 -0600

    passed all tests for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const today = new Date();
+  const year = today.getFullYear();
+  arr.map(person => {
+    if (!person.yearOfDeath) {
+      return person['age'] = year - person.yearOfBirth;
+    } else {
+      return person['age'] = person.yearOfDeath - person.yearOfBirth;
+    }
+  })
+  const sorted = arr.sort((a, b) => a.age < b.age ? 1 : -1);
+  sorted.splice(1);
+  return sorted[0];
 };
 
 // Do not edit below this line
